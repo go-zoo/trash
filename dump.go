42223d9f55e969a8b934280c0a74e6cdab55a65c
Jonathan <jonathan.chaput@voxlog.ca>
2015-07-29 15:27:31 -0400

    add mutex

--- dump.go
+++ dump.go
@@ -2,11 +2,17 @@ package trash
 
 import (
 	"io"
+	"sync"
 )
 
 type Dump struct {
 	trash   map[int]*Trash
 	errChan chan genErr
+}
+
+type writer struct {
+	sync.RWMutex
+	io.Writer
 }
 
 func NewDump(w io.Writer, trash ...*Trash) *Dump {
@@ -15,15 +21,17 @@ func NewDump(w io.Writer, trash ...*Trash) *Dump {
 		v.dump = d
 		d.trash[k] = v
 	}
-	go d.listen(w)
+	go d.listen(writer{Writer: w})
 	return d
 }
 
-func (d *Dump) listen(w io.Writer) {
+func (d *Dump) listen(w writer) {
 	for {
 		select {
 		case err := <-d.errChan:
+			w.Lock()
 			w.Write([]byte(err.FormatErr()))
+			w.Unlock()
 		default:
 			continue
 		}
