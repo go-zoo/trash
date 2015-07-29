9bacba3bc6044795783322b34066f08ebaf56e2c
Jonathan <jonathan.chaput@voxlog.ca>
2015-07-29 15:31:50 -0400

    add comments

--- dump.go
+++ dump.go
@@ -5,23 +5,25 @@ import (
 	"sync"
 )
 
+// Dump data structure
 type Dump struct {
 	trash   map[int]*Trash
 	errChan chan genErr
 }
 
 type writer struct {
-	sync.RWMutex
+	*sync.RWMutex
 	io.Writer
 }
 
+// NewDump generate a new dump instance and start listening for incoming err.
 func NewDump(w io.Writer, trash ...*Trash) *Dump {
 	d := &Dump{trash: make(map[int]*Trash), errChan: make(chan genErr)}
 	for k, v := range trash {
 		v.dump = d
 		d.trash[k] = v
 	}
-	go d.listen(writer{Writer: w})
+	go d.listen(writer{&sync.RWMutex{}, w})
 	return d
 }
 
